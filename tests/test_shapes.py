ed989f3452a0f70a3f0da10275cf9cbefe9937cf
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-09-26 16:21:24 +0100

    tests: pytest.lazy_fixture() > request.getfixturevalue()
    Pytest >= 8.0.0 no longer works with pytest-lazy-fixture so switched to using request.getfixturevalue()

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -31,20 +31,20 @@ def test_summarise_shape_get_fixture_value(
     ("shape", "area", "feret_diameter_max", "centroid"),
     [
         pytest.param(
-            pytest.lazy_fixture("square"),
+            "square",
             24,
             9.219544457292887,
             (4, 4),
             id="summary of square",
         ),
-        pytest.param(pytest.lazy_fixture("circle"), 12, 5.385164807134504, (4, 4), id="summary of circle"),
+        pytest.param("circle", 12, 5.385164807134504, (4, 4), id="summary of circle"),
     ],
 )
-def test_summarise_shape_lazy_fixture(
-    shape: str, area: float, feret_diameter_max: float, centroid: tuple[int, int]
+def test_summarise_shape_get_fixture_value(
+    shape: str, area: float, feret_diameter_max: float, centroid: tuple[int, int], request
 ) -> None:
     """Test the summarisation of shapes."""
-    shape_summary = summarise_shape(shape)
+    shape_summary = summarise_shape(request.getfixturevalue(shape))
     assert shape_summary[0]["area"] == area
     assert shape_summary[0]["feret_diameter_max"] == feret_diameter_max
     assert shape_summary[0]["centroid"] == centroid
