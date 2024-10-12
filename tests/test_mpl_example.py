0ae8c5663b04a6c967bb276b0a615d15673da597
Neil Shephard <n.shephard@sheffield.ac.uk>
2024-10-12 12:06:57 +0100

    feature: Adds pytest-mpl example and test

--- tests/test_mpl_example.py
+++ tests/test_mpl_example.py
@@ -5,15 +5,15 @@ import pytest
 from pytestexamples import mpl_example
 
 
-@pytest.mark.mpl_image_compare(baseline_dir="resources/img")
+@pytest.mark.mpl_image_compare(baseline_dir="baseline")
 @pytest.mark.parametrize(
-    ("n_obs", "figsize", "title", "cmap", "seed"),
+    ("n_obs", "figsize", "title", "seed"),
     [
-        pytest.param(300, (6, 6), None, "green", 3513387, id="300 points; 6x6; no title; green"),
-        pytest.param(3000, (6, 6), "Lots of points!", "green", 3513387, id="3000 points; 6x6; Lots of points; blue"),
+        pytest.param(300, (6, 6), "", 3513387, id="300 points; 6x6; no title; green"),
+        pytest.param(3000, (6, 6), "Lots of points!", 3513387, id="3000 points; 6x6; Lots of points; blue"),
     ],
 )
-def test_scatter_hist(n_obs: int, figsize: tuple[int, int], title: str, cmap: str, seed: int) -> None:
+def test_scatter(n_obs: int, figsize: tuple[int, int], title: str, seed: int) -> None:
     """Test of the scatter_hist() function."""
-    fig, _ = mpl_example.scatter(n_obs, figsize, title, cmap, seed)
+    fig, _ = mpl_example.scatter(n_obs, figsize, title, seed)
     return fig
